793db1d675330192153c5eaf4dc58dbd1f30a0b9
Dinesh648 <87705747+Dinesh648@users.noreply.github.com>
2024-07-16 11:15:00 +0530

    add rooms

--- base/views.py
+++ base/views.py
@@ -1,9 +1,18 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+rooms = [
+    {'id':1, 'name':'Find an organization'},
+    {'id':2, 'name':'Get a referral'},
+    {'id':3, 'name':'Find a job opening'},
+    {'id':4, 'name':'Get your resume reviewed'}
+]
+
+
 def home(request):
-    return render(request, 'home.html')
+    return render(request, 'home.html', {'rooms': rooms})
     # The templates folder is searched for home.html
 
-def jobs(request):
-    return render(request, 'jobs.html')
+def room(request):
+    return render(request, 'rooms.html')
