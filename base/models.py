4e0a7f16e346083298a4f04621e3bd9b3bf8b4b9
Dinesh648 <87705747+Dinesh648@users.noreply.github.com>
2024-07-16 16:14:39 +0530

    design models

--- base/models.py
+++ base/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+class Room(models.Model):
+    #host #topic #participants
+    name = models.CharField(max_length=200)
+    description = models.TextField(null=True, blank=True)
+    
+    updated = models.DateTimeField(auto_now=True) # everytime the model is changed this will log the time
+    created = models.DateTimeField(auto_now_add=True) # will save it for the first save
+    
+    def __str__(self) -> str:
+        return str(self.name)
