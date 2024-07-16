f7c55604575c23e9745f726d749edd1caa67cab0
Dinesh648 <87705747+Dinesh648@users.noreply.github.com>
2024-07-16 10:44:28 +0530

    navbar

--- base/views.py
+++ base/views.py
@@ -3,7 +3,7 @@ from django.shortcuts import render
 # Create your views here.
 def home(request):
     return render(request, 'home.html')
-
+    # The templates folder is searched for home.html
 
 def jobs(request):
     return render(request, 'jobs.html')
